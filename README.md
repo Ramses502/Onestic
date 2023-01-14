beaeec5ef772eb6a77523b0e37eec76e270c40a8
Ramses502 <ramses21793@gmail.com>
2023-01-14 18:15:30 +0100

    Terminado

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
 # Instalación
 
+Lo primero de todo es tener una versión de Python instalada en nuestra máquina. Puedes encontrar una aquí https://www.python.org/downloads/
 En una terminal, nos situamos en la carpeta del proyecto en el que vamos a trabajar. Seguidamente, introducimos los siguientes comandos:
-`pip install fastapi`
-`pip install uvicorn`
+`pip install fastapi`<br>
+`pip install uvicorn`<br>
+Estos instalarán las librerías que se han utilizado para la API REST del proyecto.
+
+# Ejecución
+
+Para ello, lo primero que tenemos que hacer es lanzar la API REST. Para hacer esto, en una terminal nos situamos en la carpeta del proyecto y escribimos los comandos:
+`cd src/API_REST`<br>
+`uvicorn ApiRest:app --reload`<br>
+Ya tenemos la API REST en funcionamiento y lista para ser llamada.
+
+Lo siguiente que debemos hacer es: Abrir un nuevo terminal desde donde lanzar el comando que ejecutará la app. Como antes, nos situamos en la carpeta del proyecto en la terminal y escribimos el siguiente comando para situarnos en la carpeta donde está el "script" para lanzar la app.
+`cd src/Logica`<br>
+Una vez situados en la carpeta del script, paso a explicar el funcionamiento de este sencillo comando:
+`python Main.py [argumentos]`<br>
+Donde está la parte de argumentos escribimos el report, de los 3 que pedía la prueba, que queremos lanzar (report1, report2 o report3). Por ejemplo:
+-`python Main.py report2` Para generar el CSV del report 2 de la prueba.
+-`python Main.py report1 report2 report3` Para generar los 3 CSVs de la prueba. (El orden de los argumentos no importa)
+Y con esto generaría los CSVs nombrados en la carpeta Data del proyecto que uso para almacenar todos los CSVs. Obviamente en un caso real, el CSV se guardaría en otra carpeta de la máquina que tu le indicases o directamente mapearíamos el CSV a una BD.
+
+# Sobre mi experiencia haciendo la prueba
+
+Primero voy a contar el porqué elegí Python. La verdad es que me daba igual hacerlo en Python o JS (que eran las opciones que se recomendaban junto a php) ya que he programado en los dos lenguajes haciendo proyectos en la carrera (NodeJS para una asignatura, TypeScript + Angular para un proyecto y Python + PyQt para otro). Añadir que se más lenguajes como Java, C#, C, etc... Una vez llegados a este punto, quería que la prueba me sirviese para aprender. Leído el enunciado entendí que iba a trabajar con archivos CSV, algo que ya había hecho con JS y más o menos ya sé como funciona y quería aprender como Python trataba los CSV. Así que me decidí simplemente por eso.
+
+Las dificultades que he encontrado son todas relacionadas a como trata Python los CSV. Por ejemplo el como poder extraer solo producto de la columna productos de orders.csv. Al final, entre el poco tiempo que he tenido por trabajos de la universidad y exámenes no he podido dedicarle el tiempo que me hubiese gustado para mejorar, quizás, la eficiencia con la que lo he hecho y haceros una interfaz con PyQt o Tkinter. Decir también que con una BD a la que poder mapear ciertos datos de los CSV hubiese sido infinitamente más facil porque no tendría que pensar en crear tablas en Python para luego juntarlas y transformarlas en el CSV final, sino que lo mapeo y de la BD extraigo la información ya estructurada.
+
+El porqué de mi código lo comento en el mismo (aunque la guía del buen programador indica que no hay que hacer comentarios en el código, yo los hago para que sea más rápido de entender).
+
+# Bibliografía que he usado para documentarme
+
+-Para tratar argumentos de entrada por comandos: https://www.youtube.com/watch?v=4ZcX5Prfix0&ab_channel=kipunaec
+-Para tratar los CSV como tablas: https://stackoverflow.com/questions/62480335/querying-csv-files-in-python-like-sql
+-Como ordenar una tabla en Python: https://www.geeksforgeeks.org/how-to-sort-data-by-column-in-a-csv-file-in-python/
+
+El resto de cosas que he aplicado son cosas que aprendí durante el desarrollo de mi proyecto en Python en la universidad.
+
+
--- src/API_REST/ApiRest.py
+++ src/API_REST/ApiRest.py
@@ -9,7 +9,8 @@ app = FastAPI()
 def report1():
     arraysOfProducts = []
     arrayIDsOrders = []
-
+    
+    #Cargo el CSV de pedidos y lo itero para obtener un array de IDs de los pedidos y una array de arrays de los productos.
     with open('../../Data/orders.csv', newline='') as csvfile:
         readerOrders = csv.DictReader(csvfile)
         for orders in readerOrders:
@@ -17,11 +18,14 @@ def report1():
             arraysOfProducts.append(orders['products'].split(" "))
         csvfile.close()
 
+    #Cargo el CSV e inicializo las varibales que voy a usar más tarde.
     productsTable = csvtable('../../Data/products.csv')
     tableResult = {}
     rowIterator = 0
     costOfOrder = 0
 
+    #Itero la array de arrays creada al principio y a su vez itero las arrays de dentro de la misma. Para cada iteración de las arrays saco el coste del producto
+    #y lo voy sumando al coste total de ese pedido. Al final hago una tabla de IDs de los pedidos con su respectivo coste total calculado.
     for order in arraysOfProducts:
         for product in order:
             if product in productsTable.keys(): 
@@ -31,6 +35,8 @@ def report1():
                 tableResult[idOrder] = costOfOrder
         rowIterator = rowIterator + 1
         costOfOrder = 0
+
+    #Se llama a la función que genera el CSV con la tabla resultante a faltas de concatenar a la tabla los IDs de las ordenes.   
     writeCSV(1, arrayIDsOrders, tableResult)
 
     return "order_prices.csv generated"
@@ -38,20 +44,24 @@ def report1():
 @app.get("/report2")
 def report2():
     tableResult = {}
-
+    
+    #Cargo los dos CSV que voy a usar en tablas para poder extraer sus datos.
     ordersTable = csvtable('../../Data/orders.csv')
-    customersTable = csvtable('../../Data/customers.csv')
     productsTable = csvtable('../../Data/products.csv')
 
+    #Creo he inicializo el array con todas las "posiciones" de la misma que voy a usar. Para ello las inicializo metiendoles la cadena vacía.
     arrayPorductsCustomers = []
     for product in productsTable.keys():
         arrayPorductsCustomers.append("")
 
+    #Itero las tablas de productos y pedidos para comprobar de cada orden si el producto está en la columna products y así recoger su comprador.
+    #Este comprador lo añadimos (si no está ya añadido) a una array donde cada posición representa a un producto.
     for product in productsTable.keys():
         for order in ordersTable.keys():
-            if product in ordersTable[order]['products']:
+            if product in ordersTable[order]['products'] and not(ordersTable[order]["customer"] in str(arrayPorductsCustomers[int(product)])):
                 arrayPorductsCustomers[int(product)] = str(arrayPorductsCustomers[int(product)]) + " " + ordersTable[order]["customer"]
-
+    
+    #Generamos la tabla resultante y la pasamos al generador de CSVs
     for product in productsTable:
         tableResult[product] = arrayPorductsCustomers[int(product)]
         
@@ -62,14 +72,17 @@ def report2():
 @app.get("/report3")
 def report3():
 
+    #Variables que voy a usar
     arrayIDsCustomers = []
     arraysOfProducts = []
     tableResult = {}
     tableCustomerProducts = {}
 
+    #Cargo como tablas los CSV que voy a usar
     customersTable = csvtable('../../Data/customers.csv')
     productsTable = csvtable('../../Data/products.csv')
 
+    #Como en el primer endpoint, extraigo la columna de clientes y la columna de productos como array de arrays.
     with open('../../Data/orders.csv', newline='') as csvfile:
         readerOrders = csv.DictReader(csvfile)
         for orders in readerOrders:
@@ -77,19 +90,24 @@ def report3():
             arraysOfProducts.append(orders['products'].split(" "))
         csvfile.close() 
 
+    #Inicializo con las posiciones que voy a usar la tabla resultante y la tabla auxiliar de clientes-productos que voy a utilizar.
     rowIterator = 0
     for customer in arrayIDsCustomers:
         tableCustomerProducts[customer] = []
         tableResult[customer] = 0
 
+    #Iteramos la tabla de clientes para que por cada cliente se le añadan los productos que ha comprado.
     for customer in arrayIDsCustomers:
         tableCustomerProducts[customer] += arraysOfProducts[rowIterator]
         rowIterator = rowIterator + 1
     
+    #Iteramos la tabla creada anteriormente e iteramos cada array de productos de cada posición. Por cada iteración de las arrays de productos vamos
+    #sumando para ese cliente lo que le ha costado cada producto y así obtenemos el total que se ha gastado.
     for customer in tableCustomerProducts.keys():
         for product in tableCustomerProducts[customer]:
             tableResult[customer] += float(productsTable[product]['cost'])
 
+    #Con la tabla resultante llamamos al generador de CSVs.
     writeCSV(3, customersTable, tableResult)
 
     return "customer_ranking.csv generated"
@@ -104,7 +122,7 @@ def csvtable(file):     # Read CSV file into 2-D dictionary
     for line in f.readlines():
         values = line.strip().split(',')            # Get current row
         for column,value in zip(columns,values):
-            if column == 'id':                    # table['TREX'] = {}
+            if column == 'id':                      # table['TREX'] = {}
                 key = value
                 table[key] = {}
             else:
@@ -114,35 +132,53 @@ def csvtable(file):     # Read CSV file into 2-D dictionary
     return table
 
 def writeCSV(report, AdditionalData, tableResult):
+
+    #Cuando se le llama para el report1
     if report == 1:
+        #Abrimos un writer con el CSV a generar.
         with open('../../Data/order_prices.csv', 'w', newline='') as file:
             writer = csv.writer(file)
 
+            #Escribimos el nombre de las columnas
             writer.writerow(["id", "total"])
+            #Iteramos en el array de datos adicionales (en este caso le he pasado el array de IDs de pedidos) para extraerlos para
+            #la columna "id" y como indice para la tabla resultante y asi poder printear el id de la orden y cuanto ha costado.
             for idOrder in AdditionalData:
                 if idOrder in tableResult.keys():
                     writer.writerow([idOrder, tableResult[idOrder]])
     
+    #Cuando se le llama para el report2
     elif report == 2:
+        #Abrimos un writer con el CSV a generar.
         with open('../../Data/product_customers.csv', 'w', newline='') as file:
             writer = csv.writer(file)
 
+            #Escribimos el nombre de las columnas
             writer.writerow(["id", "customer_ids"])
+            #Iteramos la tabla resultante para sacar los IDs de los productos y poder llenar la columna "id" y estos mismos los usamos
+            #para sacar los IDs de los clientes que los compraron.
             for product in tableResult.keys():
                 writer.writerow([product, tableResult[product]])
 
     else:
+        #Abrimos un writer con el CSV a generar.
         with open('../../Data/customer_ranking.csv', 'w', newline='') as file:
             writer = csv.writer(file)
 
+            #Escribimos el nombre de las columnas
             writer.writerow(["id", "name", "lastname", "total"])
+            #Iteramos en la tabla resultante para sacar el ID de los clientes y su gasto total, también usamos la tabla adicional que le he pasado
+            #para extraer el nombre y apellidos de cada cliente.
             for customer in tableResult:
                 writer.writerow([customer, AdditionalData[customer]['firstname'], AdditionalData[customer]['lastname'], tableResult[customer]])
         
+        #Una vez ya hemos generado el CSV lo volvemos a cargar, esta vez para ordenarlo. Lo hago porque de la forma que trataba los CSV no 
+        #me permitia ordenarlo.
         with open('../../Data/customer_ranking.csv', 'r', newline='') as f_input:
             csv_input = csv.DictReader(f_input)
             data = sorted(csv_input, key=lambda row: (float(row['total'])), reverse=True)
 
+        #Sobreescribimos el CSV esta vez ya ordenado por gasto.
         with open('../../Data/customer_ranking.csv', 'w', newline='') as f_output:    
             csv_output = csv.DictWriter(f_output, fieldnames=csv_input.fieldnames)
             csv_output.writeheader()
